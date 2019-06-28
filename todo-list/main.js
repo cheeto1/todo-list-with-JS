89399faa88441f8e1f18bde39b593161d6e1c819
Che To <chekinto@hotmail.com>
2019-06-28 16:44:53 +0100

    To do list made with Javascript

--- todo-list/main.js
+++ todo-list/main.js
@@ -1,7 +1,9 @@
 const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos')
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
-const generateTemplate = todo => {
+
+const createTodo = todo => {
   const html = `
   <li class="list-group-item d-flex justify-content-between align-items-center">
   <span>${todo}</span>
@@ -18,9 +20,29 @@ addForm.addEventListener('submit', event => {
   const todo = addForm.add.value.trim();
 
   if(todo.length){
-    generateTemplate(todo);
+    createTodo(todo);
     addForm.reset()
   }
+});
+// DELETE-SECTION
+list.addEventListener('click', e => {
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+});
 
+const filterTodos = (term) => {
+  Array.from(list.children)
+  .filter(todo => !todo.textContent.toLowerCase().includes(term))
+  .forEach(todo => todo.classList.add('filtered'));
 
+  Array.from(list.children)
+  .filter(todo => todo.textContent.toLowerCase().includes(term))
+  .forEach(todo => todo.classList.remove('filtered'));
+};
+
+// SEARCH_SECTION
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
